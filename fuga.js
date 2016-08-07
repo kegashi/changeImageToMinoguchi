34408cbfde74047a12686ca2e3e6703bf3740aad
zukky <zukky@ht.sfc.keio.ac.jp>
2016-08-08 00:53:48 +0900

    first commit

---
+++ fuga.js
@@ -0,0 +1,16 @@
+$(function(){
+	//window.alert("hoge");
+
+	// ページ内を加工
+	
+	/*				
+	$("body").html(
+		$("body").html().replace( /ー/g, "━━━(ﾟ∀ﾟ)━━━" )
+	);
+	$("body").html(
+		$("body").html().replace( /<img src=*>/g, "<img src='http://baku.sfc.keio.ac.jp/wp-content/uploads/1212.jpg'>")
+	);
+	*/
+	$("img").attr("src","http://baku.sfc.keio.ac.jp/wp-content/uploads/1212.jpg")
+							
+});
